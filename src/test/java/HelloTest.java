50f9934297d90fef88e2a94ad9288bb5845df9ea
fatimakucuk <72092041+fatimakucuk@users.noreply.github.com>
2021-05-30 23:04:03 +0200

    2 line ekledim

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,8 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("bos bos sayfa olmaz icini doldur");
+
+        //day4
+        //yeni eklendi
     }
 }
